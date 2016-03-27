eb18032cd9e4ae0fe1499b44c5ff09268747467c
James Chambers <james@jameschambers.com>
2016-03-27 17:06:44 -0500

    Sample project with configuration

---
+++ ConfigurationSample/src/ConfigurationSample/Configuration/AzureStorageSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationSample.Configuration
+{
+    public class AzureStorageSettings
+    {
+        public string AzureStorage { get; set; }
+        public bool EnableAzureQueueService { get; set; }
+    }
+}
---
+++ ConfigurationSample/src/ConfigurationSample/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.OptionsModel;
+using ConfigurationSample.Configuration;
+
+namespace ConfigurationSample.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private AzureStorageSettings _settings;
+        public HomeController(IOptions<AzureStorageSettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.StorageKey = _settings.AzureStorage;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
